7df08066ea513fc71d3f3a014f7ea10b2cb752d5
arronkennedy <aktkenpro@gmail.com>
2026-03-09 18:28:26 +0700

    initial commit

---
+++ test/test_calculator.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app.calculator import add, subtract, multiply, divide
+import pytest
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+
+def test_multiply():
+    assert multiply(4, 2) == 8
+
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10, 0)
